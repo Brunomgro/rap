5efd818e1447728e3237ea973f62c76b40f6a62f
Brunomgro <brunomgro@gmail.com>
2023-12-30 15:36:21 -0300

    criando primeiro spring

---
+++ SpotyRap/src/main/java/com/rap/streamming/SpotyRap/SpotyRapApplication.java
@@ -0,0 +1,13 @@
+package com.rap.streamming.SpotyRap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotyRapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotyRapApplication.class, args);
+	}
+
+}
